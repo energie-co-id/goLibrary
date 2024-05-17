4b54679651c4bc9e28273fc748d40a70267727c9
Kevin Rionaldo <kevinhutabarat4@gmail.com>
2024-05-17 16:55:05 +0700

    command consumer

--- kafkaLib/kafkaLib.go
+++ kafkaLib/kafkaLib.go
@@ -1,9 +1,6 @@
 package kafkaLib
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
@@ -26,35 +23,35 @@ func Producer(brokersUrl string, topic string, message string) error {
 	return err
 }
 
-func Consumer(brokersUrl string, topic string) (string, error) {
-	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": brokersUrl,
-		"group.id":          "myGroup",
-		"auto.offset.reset": "earliest",
-	})
+// func Consumer(brokersUrl string, topic string) (string, error) {
+// 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+// 		"bootstrap.servers": brokersUrl,
+// 		"group.id":          "myGroup",
+// 		"auto.offset.reset": "earliest",
+// 	})
 
-	if err != nil {
-		panic(err)
-	}
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	c.SubscribeTopics([]string{topic}, nil)
+// 	c.SubscribeTopics([]string{topic}, nil)
 
-	// A signal handler or similar could be used to set this to false to break the loop.
-	run := true
-	message := ""
-	for run {
-		msg, err := c.ReadMessage(time.Second)
-		if err == nil {
-			message = string(msg.Value)
-		} else if !err.(kafka.Error).IsTimeout() {
-			// The client will automatically try to recover from all errors.
-			// Timeout is not considered an error because it is raised by
-			// ReadMessage in absence of messages.
-			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
-		}
-	}
+// 	// A signal handler or similar could be used to set this to false to break the loop.
+// 	run := true
+// 	message := ""
+// 	for run {
+// 		msg, err := c.ReadMessage(time.Second)
+// 		if err == nil {
+// 			message = string(msg.Value)
+// 		} else if !err.(kafka.Error).IsTimeout() {
+// 			// The client will automatically try to recover from all errors.
+// 			// Timeout is not considered an error because it is raised by
+// 			// ReadMessage in absence of messages.
+// 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+// 		}
+// 	}
 
-	c.Close()
+// 	c.Close()
 
-	return message, err
-}
+// 	return message, err
+// }
