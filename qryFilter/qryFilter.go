f454d0ce8d9128cb9e2a74dba0279c2e7abc2c5e
Kevin Rionaldo <kevin@Feirys-MacBook-Pro.local>
2024-09-06 16:33:33 +0700

    add error and fix time format qry filter

--- qryFilter/qryFilter.go
+++ qryFilter/qryFilter.go
@@ -14,11 +14,11 @@ func DateSplitFilter(date string, dateType string, from string, to string) (fina
 
 	switch dateType {
 	case "daily":
-		startTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day(), 0, 0, 0, 0, customTime.Location())
-		stopTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day(), 23, 59, 59, 99, customTime.Location())
+		startTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day(), -7, 0, 0, 0, customTime.Location())
+		stopTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day(), 16, 59, 59, 99, customTime.Location())
 	case "weekly":
-		startTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day()-6, 0, 0, 0, 0, customTime.Location())
-		stopTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day(), 23, 59, 59, 99, customTime.Location())
+		startTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day()-7, 0, 0, 0, 0, customTime.Location())
+		stopTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day(), 16, 59, 59, 99, customTime.Location())
 	case "monthly":
 		startTime = time.Date(customTime.Year(), customTime.Month(), 1, 0, 0, 0, 0, customTime.Location())
 		stopTime = time.Date(customTime.Year(), startTime.Month()+1, startTime.Day()-1, 23, 59, 59, 99, customTime.Location())
@@ -31,8 +31,8 @@ func DateSplitFilter(date string, dateType string, from string, to string) (fina
 		if error != nil {
 			return "error", "error"
 		}
-		startTime = time.Date(from.Year(), from.Month(), from.Day(), 0, 0, 0, 0, from.Location())
-		stopTime = time.Date(to.Year(), to.Month(), to.Day(), 23, 59, 59, 99, to.Location())
+		startTime = time.Date(from.Year(), from.Month(), from.Day(), -7, 0, 0, 0, from.Location())
+		stopTime = time.Date(to.Year(), to.Month(), to.Day(), 16, 59, 59, 99, to.Location())
 	}
 
 	return startTime.Format(time.RFC3339), stopTime.Format(time.RFC3339)
