8e5c660800edde48ba76a827c1dff184dd59ac5b
Kevin Rionaldo <kevinhutabarat4@gmail.com>
2024-09-24 16:31:18 +0700

    update output format datesplitfilter to time.time

--- qryFilter/qryFilter.go
+++ qryFilter/qryFilter.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func DateSplitFilter(date string, dateType string, from string, to string) (finalStartTime string, finalStopTime string, err error) {
+func DateSplitFilter(date string, dateType string, from string, to string) (finalStartTime time.Time, finalStopTime time.Time, err error) {
 	customTime, err := time.Parse("2006-01-02", date)
 	if err != nil {
 		customTime = time.Now()
@@ -26,22 +26,22 @@ func DateSplitFilter(date string, dateType string, from string, to string) (fina
 	case "yearly":
 		year, err := strconv.ParseInt(date, 10, 64)
 		if err != nil {
-			return "error", "error", err
+			return startTime, stopTime, err
 		}
 		startTime = time.Date(int(year), 0, 1, 0, 0, 0, 0, customTime.Location())
 		stopTime = time.Date(int(year), 13, startTime.Day()-1, 23, 59, 59, 99, customTime.Location())
 	default:
 		from, err := time.Parse("2006-01-02", from)
 		if err != nil {
-			return "error", "error", err
+			return startTime, stopTime, err
 		}
 		to, error := time.Parse("2006-01-02", to)
 		if error != nil {
-			return "error", "error", err
+			return startTime, stopTime, err
 		}
 		startTime = time.Date(from.Year(), from.Month(), from.Day(), -7, 0, 0, 0, from.Location())
 		stopTime = time.Date(to.Year(), to.Month(), to.Day(), 16, 59, 59, 99, to.Location())
 	}
 
-	return startTime.Format(time.RFC3339), stopTime.Format(time.RFC3339), nil
+	return startTime, stopTime, nil
 }
