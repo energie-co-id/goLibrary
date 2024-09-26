b3add39a7786717ad2ab06de6fa4ead31dfb4b44
Kevin Rionaldo <kevinhutabarat4@gmail.com>
2024-09-26 16:13:30 +0700

    fix monthly and weekly at date split filter

--- qryFilter/qryFilter.go
+++ qryFilter/qryFilter.go
@@ -18,11 +18,13 @@ func DateSplitFilter(date string, dateType string, from string, to string) (fina
 		startTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day(), -7, 0, 0, 0, customTime.Location())
 		stopTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day(), 16, 59, 59, 99, customTime.Location())
 	case "weekly":
-		startTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day()-7, 0, 0, 0, 0, customTime.Location())
+		startTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day()-6, -7, 0, 0, 0, customTime.Location())
 		stopTime = time.Date(customTime.Year(), customTime.Month(), customTime.Day(), 16, 59, 59, 99, customTime.Location())
 	case "monthly":
-		startTime = time.Date(customTime.Year(), customTime.Month(), -7, 0, 0, 0, 0, customTime.Location())
-		stopTime = time.Date(customTime.Year(), startTime.Month()+1, startTime.Day()-1, 16, 59, 59, 99, customTime.Location())
+		startTime = time.Date(customTime.Year(), customTime.Month(), 1, -7, 0, 0, 0, customTime.Location())
+
+		stopTime = time.Date(customTime.Year(), customTime.Month()+1, 1, 0, 0, 0, 0, customTime.Location())
+		stopTime = time.Date(stopTime.Year(), stopTime.Month(), stopTime.Day()-1, 16, 59, 59, 99, customTime.Location())
 	case "yearly":
 		year, err := strconv.ParseInt(date, 10, 64)
 		if err != nil {
